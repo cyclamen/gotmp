72c88ecd470276a45eb823639eabfe1099a800cc
cyclamen <pitt.chang@gmail.com>
2017-11-13 17:58:59 +0800

    :pencil2: Add new log output

--- main.go
+++ main.go
@@ -43,5 +43,6 @@ func main() {
 	Log.Info("Configure release mode is %v", conf.Conf.ReleaseMode)
 	Log.Info("Configure log level is %s", conf.Conf.LogLevel)
 	Log.Info("Configure app name is %s", conf.Conf.App.Name)
+	Log.Info("Configure app backup number is %d", conf.Conf.App.BackupNumber)
 
 }
